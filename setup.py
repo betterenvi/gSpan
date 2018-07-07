2403d55e528818ec3c335ea9e9828c82357a4b7a
CQY <qychen@pku.edu.cn>
2018-07-07 13:56:45 +0800

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gspan_mining",
-    version="0.1",
+    version="0.2.0",
     author="Qingying Chen",
     author_email="qychen.pku@gmail.com",
     description="Implementation of frequent subgraph mining algorithm gSpan",
@@ -15,6 +15,7 @@ setuptools.setup(
     url="https://github.com/betterenvi/gSpan",
     packages=['gspan_mining'],
     classifiers=(
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
